24f23c16d2487c37bfd6d82b27b0c481c553c77c
Hager-h <hagersalahali3@gmail.com>
2023-09-17 12:01:10 -0700

    Aboutchange

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -6,17 +6,28 @@ import { TbPlayerPlayFilled } from "react-icons/tb";
 import { AiOutlineHeart } from "react-icons/ai";
 import { BiLibrary } from "react-icons/bi";
 import { HiOutlineInformationCircle } from "react-icons/hi";
+import { FaPrayingHands } from "react-icons/fa";
+import { FaRosary } from "react-icons/fa";
 
+// داخل العنصر الذي ترغب في وض
 export default function Sidebar() {
   return (
     <div className="sidebar">
       <div className="circle flex">
         <img src={sliderImg} alt="" />
       </div>
+
       <div className="icons">
-        <NavLink className="link" activeClassName="active"to="/">
+        <NavLink className="link" to="/القران الكريم">
           <span>
-            <TbPlayerPlayFilled />
+            <BiLibrary />
+          </span>{" "}
+          <span>القران الكريم</span>
+        </NavLink>
+
+        <NavLink className="link" activeclassname="active" to="/">
+          <span>
+            <FaPrayingHands />
           </span>{" "}
           <span>الرقية الشرعية</span>
         </NavLink>
@@ -26,25 +37,15 @@ export default function Sidebar() {
           </span>{" "}
           <span>الأذكار</span>
         </NavLink>
-        <NavLink className="link" to="/القران الكريم">
-          <span>
-            <BiLibrary />
-          </span>{" "}
-          <span>القران الكريم</span>
-        </NavLink>
+
       </div>
       <div className="sign-out">
-
-
-      <NavLink className="link about" to="/about">
-      <span>
+        <NavLink className="link about" to="/about">
+          <span>
             <HiOutlineInformationCircle />
           </span>{" "}
           <span> عــن المـوقع</span>
         </NavLink>
-
-
-
       </div>
     </div>
   );
